c2f600a97aa7758bdcd10a5cc25e6f5704a22462
Hein Dauven <heindauven@gmail.com>
2018-10-18 22:19:44 +0200

    Add passport as middleware and setup configuration reference.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -24,6 +25,13 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Base route
 app.get("/", (req, res) => res.send("hello, test"));
 
 // Use routes
